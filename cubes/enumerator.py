42f833097c234a3a64ba77f963ed062602ff8540
Ladislav Dubravský <dubravsky90@gmail.com>
2018-11-08 20:15:58 +0100

    Added from_bitarray

--- cubes/enumerator.py
+++ cubes/enumerator.py
@@ -1,6 +1,7 @@
 import numpy as np
+import csv
 
-# mapping found by backtracking optimizer
+# mapping of pairs to bitarray positions found by backtracking optimizer
 MAPPING = {"uFr": 42, "ubR": 44, "uBl": 46, "ufL": 48,
            "ufR": 19, "uBr": 21, "ubL": 23, "uFl": 25,
            "uf": 0, "ur": 2, "ub": 4, "ul": 6,
@@ -27,10 +28,10 @@ for _face in 'UFRDBL':
         if _face in k or (len(k) == 2 and _face.lower() == k[1])))
 
 # single cubies with number codes for cubelist representation of bandage shape
-SLOTS = ["ubl", "ub", "ubr", "ul", "u", "ur", "ufl", "uf", "ufr",
-         "bl", "b", "br", "l", "c", "r", "fl", "f", "fr",
-         "dbl", "db", "dbr", "dl", "d", "dr", "dfl", "df", "dfr"]
-CUBIES = {c: i for i, c in enumerate(SLOTS)}
+CUBIES = ["ubl", "ub", "ubr", "ul", "u", "ur", "ufl", "uf", "ufr",
+          "bl", "b", "br", "l", "c", "r", "fl", "f", "fr",
+          "dbl", "db", "dbr", "dl", "d", "dr", "dfl", "df", "dfr"]
+SLOTS = {c: i for i, c in enumerate(CUBIES)}
 
 # cycles and pairs
 CYCLES = [['uFr', 'ubR', 'uBl', 'ufL'], ['uFr', 'Ubr', 'dBr', 'Dfr'],
@@ -43,6 +44,7 @@ CYCLES = [['uFr', 'ubR', 'uBl', 'ufL'], ['uFr', 'Ubr', 'dBr', 'Dfr'],
           ['ru', 'rb', 'rd', 'rf'], ['df', 'dr', 'db', 'dl'],
           ['bu', 'br', 'bd', 'bl'], ['lu', 'lb', 'ld', 'lf']]
 PAIRS = set(p for c in CYCLES for p in c)
+assert PAIRS == MAPPING.keys()
 
 
 def enumerate_analytic():
@@ -56,7 +58,7 @@ def enumerate_analytic():
     3. Either end the process or return to step 2.
     We'll call any partition of 3x3x3 cube into blocks resulting from the
     process above a bandage shape.
-    With a few other tweaks to be spotted below, how many distinct bandage
+    With several important tweaks to be spotted below, how many distinct bandage
     shapes are there?
     """
     p1 = 1
@@ -64,23 +66,27 @@ def enumerate_analytic():
     p3 = 1 + 2*p2*p1 - 1
     p22 = 1 + 2*p2**2 - 1
     p32 = 1 + 2*p22*p2 + p3**2 - p2**3 - 2*p2**2 + 1
-    p33 = p32*p3
-    p222 = 1 + 3*p22**2 - 3*p2**4 + 1
-    p322 = 1 + 2*p32**2 + 2*p222*p22 - p3**4 - 4*p2**2*p22**2 - p22**3 + 2*p2**4 + 2*p2**6 - 1
-    p332 = p33**2 + p322*p32 - p32**2*p3**2
+    p33 = 1 + p32*p3
+    p22c = 1 + 2*p2 - 1
+    p222 = 1 + 3*p22*p22c - 3*p2**3 + 1
+    p3c = 1
+    p32c = 1 + 2*p22c*p2 + p3*p3c - p2**2 - 2*p2**1 + 1
+    p322 = 1 + 2*p32*p32c + 2*p222*p22 - p3**3 - 4*p2**2*p22c*p22 - p22**2*p22c + 2*p2**3 + 2*p2**5 - 1
+    p33c = 1 + p32c*p3
+    p332 = 1 + p33*p33c + p322*p32 - p32*p32c*p3**2 # should be 54354
     p333 = 1 + p332*p33
-    return p333
+    return p333 # 7446498
 
 
-def get_cubies(pair):
-    """ Return the two cubies belonging to a pair. """
+def get_slots(pair):
+    """ Return slots of the two cubies belonging to a pair. """
     c1 = "".join(c for c in pair if c.islower())  # edge
-    c1 = c1 if c1 in CUBIES.keys() else c1[::-1]
+    c1 = c1 if c1 in SLOTS.keys() else c1[::-1]
     if len(pair) == 3:
         c2 = pair.lower()  # corner
     else:
         c2 = pair[0]  # face center
-    return c1, c2
+    return SLOTS[c1], SLOTS[c2]
 
 
 def into_bitarray(cubelist, mapping):
@@ -104,8 +110,8 @@ def into_bitarray(cubelist, mapping):
     """
     rep = 0
     for pair in PAIRS:
-        c1, c2 = get_cubies(pair)
-        if cubelist[CUBIES[c1]] == cubelist[CUBIES[c2]] > 0:
+        s1, s2 = get_slots(pair)
+        if cubelist[s1] == cubelist[s2] > 0:
             rep += 2 ** mapping[pair]
     return np.uint64(rep)
 
@@ -127,10 +133,32 @@ def into_bitarray_gencode(mapping):
     code = ["def into_bitarray_fast(c):\n    res=0\n"]
     cond = "    if c[{0}] == c[{1}]:\n        res += {2}\n"
     for pair in PAIRS:
-        c1, c2 = get_cubies(pair)
-        code.append(cond.format(CUBIES[c1], CUBIES[c2], 2 ** mapping[pair]))
+        c1, c2 = get_slots(pair)
+        code.append(cond.format(c1, c2, 2 ** mapping[pair]))
     code.append("    return np.uint64(res)")
     return "".join(code)
 
+
 # define into_bitarray_fast function whose body is just lot of magic constants
 exec(into_bitarray_gencode(MAPPING))
+
+
+def from_bitarray(bitarray, mapping, pprint=True):
+    """ Changes bitarray representation of bandage shape into human readable
+    cubelist representation. """
+    glued_pairs = []
+    inv = {v: k for k, v in mapping.items()}
+    for i in range(64):  # retrieve list of glued pairs
+        if np.bitwise_and(bitarray, np.uint64(2**i)) != 0:
+            glued_pairs.append(inv[i])
+    cubelist = np.array(range(1, 28))
+    for pair in glued_pairs:
+        s1, s2 = get_slots(pair)
+        block1 = np.where(cubelist == cubelist[s1])[0]
+        cubelist[block1] = cubelist[s2]  # merge blocks
+    if pprint:  # pretty print
+        for i, block in enumerate(cubelist):
+            if i % 3 == 0:
+                print("\n", ((8 - i // 3) % 3)*" ", end="")
+            print(str(block).zfill(2), end=" ")
+    return list(cubelist)
